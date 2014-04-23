ce034df1e31022ffdb40788bdcd6c616cfbde262
Andrew <andrew@onlinetouch.nl>
2014-04-24 01:01:45 +0900

    Clean-up and proper naming.

--- overflow-style-change/src/Main.hx
+++ overflow-style-change/src/Main.hx
@@ -2,7 +2,7 @@ package ;
 
 /**
  * ...
- * @author AndrewP
+ * @author 
  */
 
 import prime.gui.core.UIWindow;
@@ -18,7 +18,7 @@ class Main extends UIWindow
 		prime.gui.display.Window.startup( function (s) return new Main(s) );
 	}
 
-	public function new(target:prime.gui.display.Stage, id:String = null)
+	public function new(target:prime.gui.display.Stage, id:String = "Main")
 	{
 		super(target, id);
 	}
@@ -29,33 +29,31 @@ class Main extends UIWindow
 
 		// You need an extra container to get scroll stuff working, as parent mouse events are used.
 		// I.E without m container, the UIWindows mouse events would be used which doesnt work.
-		var m:UIContainer  = new UIContainer("parent");
+		var proxy:UIContainer  = new UIContainer("proxy");
 		var parent:UIContainer  = new UIContainer("parent");
 		var child:UIContainer  = new UIContainer("child");
 
 
 		parent.attach( child );
-		m.attach(parent);
-		attach( parent );
+		proxy.attach(parent);
+		attach( proxy );
 
 		toggleStyles.bind(parent) .on( parent.userEvents.mouse.click, this );
+
 		on = true;
-		toggleStyles(parent);
+		parent.styleClasses.add( "scrolling" );
 	}
 
 	var on : Bool;
 	private function toggleStyles( parent:UIContainer )
 	{
 		on = !on;
-		//parent.styleClasses.remove( "zoomed" );
-		//parent.styleClasses.remove( "unzoomed" );
 		parent.styleClasses.removeAll();
+
 		if ( on )
-			parent.styleClasses.add( "zoomed ");
+			parent.styleClasses.add( "scrolling" );
 		else
-			parent.styleClasses.add("unzoomed");
-		for( s in parent.styleClasses )
-		trace(s);
+			parent.styleClasses.add( "visible" );
 	}
     
 }
